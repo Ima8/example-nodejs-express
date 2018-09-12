e7a959f72691e4629562a4d50138e9516eba399a
Issaret Prachitmutita <imac.monochrome@gmail.com>
2018-09-12 20:54:17 +0700

    inital project

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+
+function format(seconds){
+  function pad(s){
+    return (s < 10 ? '0' : '') + s;
+  }
+  var hours = Math.floor(seconds / (60*60));
+  var minutes = Math.floor(seconds % (60*60) / 60);
+  var seconds = Math.floor(seconds % 60);
+
+  return pad(hours) + ':' + pad(minutes) + ':' + pad(seconds);
+}
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.status(200).json({
+    uptime:format(process.uptime())
+  })
+});
+
+module.exports = router;
